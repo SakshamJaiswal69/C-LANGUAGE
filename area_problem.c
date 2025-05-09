e6ed3813dbbf7c141f56caa2b50a88fa935c4126
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-08 22:23:32 -0700

    Add files via upload

---
+++ area_problem.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int length,breadth;
+    //int breadth;
+    printf("Enter the length\n");
+    scanf("%d",&length);
+
+    printf("Enter the breadth\n");
+    scanf("%d",&breadth);
+
+    printf("the area of this rectangle is %d",length*breadth);
+    return 0;
+}
