785ddd8edc0edd482f202469e70ca04b88d78bc2
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-19 07:33:56 -0700

    gndf

---
+++ Union_pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+union high{
+    int a;
+    int b;
+};
+int main()
+{
+    union high * ptr;
+    union high var ;
+    printf("enter the value:\n");
+    scanf("%d",&var.a);
+    var.a ;
+    ptr = &var;
+    printf("The Value of a is :%d",ptr ->a);
+    return 0;
+}
---
+++ ifelse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter your age barkhurdaar : ");
+    scanf("%i ",&a);
+
+    if(a>=19)
+    {
+        printf("you are eligible for voting");
+    }
+    else
+    {
+        printf("you are not Eligible");
+    }
+    return 0;
+}
