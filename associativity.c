37b8eeb29f3821d0accef1fbb5ccb6e63eb89b20
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-19 07:22:42 -0700

    today codes

---
+++ associativity.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int a = 2;
+    int b = 3;
+    int c = 2;
+
+    printf("the value is %d", a*b/c +2);
+    return 0;
+}
---
+++ type_casting.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int n  = 45;
+    float m =32.43;
+    n = (int) m;
+    printf("%d\n", n);
+    return 0;
+}
