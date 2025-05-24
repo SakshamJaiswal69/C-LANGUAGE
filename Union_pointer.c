e430f7b5178aff7155cca97f6e66d88d7cb58828
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-23 22:21:59 -0700

    Update Union_pointer.c

--- Union_pointer.c
+++ Union_pointer.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 union high{
     int a;
@@ -13,4 +14,21 @@ int main()
     ptr = &var;
     printf("The Value of a is :%d",ptr ->a);
     return 0;
+=======
+#include <stdio.h>
+union high{
+    int a;
+    int b;
+};
+int main()
+{
+    union high * ptr;
+    union high var ;
+    printf("enter the value:\n");
+    scanf("%d",&var.a);
+    var.a ;
+    ptr = &var;
+    printf("The Value of a is :%d",ptr ->a);
+    return 0;
+>>>>>>> ea522f5577ce28428b9466e3da2065401ee02aef
 }
