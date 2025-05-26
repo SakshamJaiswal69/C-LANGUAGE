546cf949bd8abd4c62fb386fa09f1331065a7638
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-25 19:42:44 -0700

    Update associativity.c

--- associativity.c
+++ associativity.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 
 int main(){
@@ -7,4 +8,15 @@ int main(){
 
     printf("the value is %d", a*b/c +2);
     return 0;
+=======
+#include <stdio.h>
+
+int main(){
+    int a = 2;
+    int b = 3;
+    int c = 2;
+
+    printf("the value is %d", a*b/c +2);
+    return 0;
+>>>>>>> ea522f5577ce28428b9466e3da2065401ee02aef
 }
