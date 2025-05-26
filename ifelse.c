cd027328dfdaf88924e62d377a1c094bea72dc97
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-25 19:42:22 -0700

    Update ifelse.c

--- ifelse.c
+++ ifelse.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 int main(){
     int a;
@@ -13,4 +14,21 @@ int main(){
         printf("you are not Eligible");
     }
     return 0;
+=======
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter your age barkhurdaar : ");
+    scanf("%i ",&a);
+
+    if(a>=19)
+    {
+        printf("you are eligible for voting");
+    }
+    else
+    {
+        printf("you are not Eligible");
+    }
+    return 0;
+>>>>>>> ea522f5577ce28428b9466e3da2065401ee02aef
 }
