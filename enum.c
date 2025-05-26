1c749075605ba5e10a345e1075f38fa13cd84fbd
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-25 23:57:49 -0700

    Update enum.c

--- enum.c
+++ enum.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 enum weekdays{Sunday=15,Monday,Tuesday,Wednesday =40,Thursday,Friday,Saturday};
 int main(){
@@ -7,14 +6,14 @@ int main(){
     printf("the value of Tuesday are %d",g);
 
     return 0;
-=======
-#include <stdio.h>
-enum weekdays{Sunday=15,Monday,Tuesday,Wednesday =40,Thursday,Friday,Saturday};
-int main(){
-    enum weekdays g;
-    g = Thursday;
-    printf("the value of Tuesday are %d",g);
-
-    return 0;
->>>>>>> ea522f5577ce28428b9466e3da2065401ee02aef
 }
+// #include <stdio.h>
+// enum weekdays{Sunday=15,Monday,Tuesday,Wednesday =40,Thursday,Friday,Saturday};
+// int main(){
+//     enum weekdays g;
+//     g = Thursday;
+//     printf("the value of Tuesday are %d",g);
+
+//     return 0;
+
+// }
