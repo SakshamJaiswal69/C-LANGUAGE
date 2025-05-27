2d6cf6988f2a7d1a9af6e8a51bbc93e876f726ca
SakshamJaiswal69 <a6136593@gmail.com>
2025-05-26 22:06:50 -0700

    update

--- Union_pointer.c
+++ Union_pointer.c
@@ -1,4 +1,20 @@
-<<<<<<< HEAD
+
+// #include <stdio.h>
+// union high{
+//     int a;
+//     int b;
+// };
+// int main()
+// {
+//     union high * ptr;
+//     union high var ;
+//     printf("enter the value:\n");
+//     scanf("%d",&var.a);
+//     var.a ;
+//     ptr = &var;
+//     printf("The Value of a is :%d",ptr ->a);
+//     return 0;
+// }
 #include <stdio.h>
 union high{
     int a;
@@ -14,21 +30,5 @@ int main()
     ptr = &var;
     printf("The Value of a is :%d",ptr ->a);
     return 0;
-=======
-#include <stdio.h>
-union high{
-    int a;
-    int b;
-};
-int main()
-{
-    union high * ptr;
-    union high var ;
-    printf("enter the value:\n");
-    scanf("%d",&var.a);
-    var.a ;
-    ptr = &var;
-    printf("The Value of a is :%d",ptr ->a);
-    return 0;
->>>>>>> ea522f5577ce28428b9466e3da2065401ee02aef
+
 }
---
+++ practise_set_3.c
@@ -0,0 +1,17 @@
+// Explain step by step evaluation of 3*x/y-z+k,whee x=2,y=3,z=3,k=1
+// #include <stdio.h>
+
+// int main(){
+//     int x =2,y=3,z =3,k=1;
+//     float e =3*x/y-z+k;
+//     printf("the value of e is %f",e);// Ans is 0
+//     return 0;
+// }
+
+#include <stdio.h>
+
+int main(){
+    float a = 3.0 + 1;
+    printf("the value of a is %f",a);
+    return 0;
+}
