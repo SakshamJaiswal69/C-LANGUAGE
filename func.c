9edb22a8f9a2a29153f468016f1ab17ab7a42fd7
SakshamJaiswal69 <a6136593@gmail.com>
2025-07-14 19:17:34 +0530

    Create func.c

---
+++ func.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+// Funtion prototype
+// int sum(int ,int);
+// /* defination */
+//  int sum(int a,int b) 
+//     {
+//         printf("%d\n",a+b);
+//        return a+b; 
+//     }
+   
+// int main(){
+// /* calling */
+//    sum(24,26);
+//    sum(45,82);
+//    int c=81,d=9;
+//    sum(c,d);
+   
+//   return 0; 
+// }
+
+void good_morning();
+void good_afternoon();
+void good_evening();
+ int main(){
+    void good_morning(){
+        printf("Hello good morning\n");
+    }
+    void good_afternoon(){
+        printf("hello good afternoon\n");
+    }
+    void good_evening(){
+    printf("hello good evening\n");
+    
+}
+
+good_morning();
+good_afternoon();
+good_evening();
+
+ }
